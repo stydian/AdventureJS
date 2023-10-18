1826d2f93a8287e0f56a62f3b6730b8dba85b946
Gabe Diaz <stydianx@protonmail.com>
2023-10-17 19:53:00 -0500

    Bug Fixes AdventureJS
    Fixed issue with saving and loading game state by placing some function into the global scope and changing localStorage method.

--- src/game.js
+++ src/game.js
@@ -1,4 +1,4 @@
-const textElement = document.getElementById('text')
+var textElement = document.getElementById('text')
 const optionButtonsElement = document.getElementById('option-buttons')
 let state = {}
 let intro = document.querySelector('.intro');
@@ -11,116 +11,45 @@ function slideUpIntro() {
     }, 500);
 }
 
+const loadSaveButton = document.querySelector('.localSaved');
+loadSaveButton.addEventListener('click', function() {
+    const savedState = JSON.parse(localStorage.getItem('savedGameState'));
+    console.log(savedState); // This will log the saved game data to the console
+        if (savedState) {
+            var textNode = textNodes.find(textNode => textNode.id === savedState);
+            if (textNode) {
+                textElement.innerHTML = textNode.text;
+                    while (optionButtonsElement.firstChild) { // removes buttons
+                        optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+                    }
+                    textNode.options.forEach(option => {
+                        if (showOption(option)) {
+                            const button = document.createElement('button')
+                            button.innerText = option.text
+                            button.classList.add('button')
+                            button.addEventListener('click', () => selectOption(option))
+                            optionButtonsElement.appendChild(button)
+                        }
+                    })
+                console.log('save found');
+            } else {
+                console.log('save not found');
+            }
+        }
+});
+
 window.addEventListener('DOMContentLoaded', () => {
 
+        const savedState = (localStorage.getItem('savedGameState'));
+        console.log(savedState); // This will log the saved game data to the console
 
-    function showTextNode(textNodeIndex) {
-        const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
-        textElement.innerText = textNode.text
-        while (optionButtonsElement.firstChild) { // removes buttons
-            optionButtonsElement.removeChild(optionButtonsElement.firstChild)
-        }
-        textNode.options.forEach(option => {
-            if (showOption(option)) {
-                const button = document.createElement('button')
-                button.innerText = option.text
-                button.classList.add('button')
-                button.addEventListener('click', () => selectOption(option))
-                optionButtonsElement.appendChild(button)
-            }
-        })
-    }
-
-    function showOption(option) {
-        return option.requiredState == null || option.requiredState(state)
-    }
-
-    function selectOption(option) {
-        const nextTextNodeId = option.nextText;
-        state = Object.assign(state, option.setState);
-        state.textNodeIndex = nextTextNodeId;
-        if (option.saveGame) {
-            saveGame(state);
-        }
-        showTextNode(nextTextNodeId);
-    }
-
-    function saveGame(state) {
-        try {
-            const serializedState = JSON.stringify(state);
-            localStorage.setItem('savedGameState', serializedState);
-            console.log('Game saved:', state);
-            console.log('Serialized state:', serializedState);
-        } catch (error) {
-            console.error('Error while saving the game:', error);
-        }
-    }
-    
-/* Takes current state, add everything from `option.setState` to it
-and over-rides everything thats there. So if "Whatever item" is true, but false in `option.setState`
-its going to set it to false in `state` and it will return a brand new object which we will set
-to the current state. */
-
-    function selectOption(option) {
+/*     function selectOption(option) {
         const nextTextNodeId = option.nextText;
         state = Object.assign(state, option.setState);
         state.textNodeIndex = nextTextNodeId; //
         saveGame(state); //
         showTextNode(nextTextNodeId);
-    }
-
-
-/* In the lines below, we use [] and {}, with the curly braces being inside the brackets to create Objects 
-inside an array which will serve as elements for dialogue and options */
-
-    const textNodes = [
-        {
-            id: 1,
-            text: 'You suddenly woke up and looked at the window to see the skies were dark. You thought to yourself "Huh? Must still be night time"... ',
-            options: [
-                {
-                    text: 'Grab your phone and stand up',
-                    setState: { phone: true },
-                    nextText: 2
-                },
-                {
-                    text: 'Go back to sleep',
-                    nextText: 2.1
-                }
-            ]
-        },
-        {
-            id: 2.1,
-            text: 'You woke up once again.. You looked to the window and saw that it was still dark outside... You found it strange so you sat uup on your bed.',
-            options: [
-                {
-                    text: 'Grab your phone and stand up',
-                    setState: { phone: true },
-                    nextText: 2
-                },
-                {
-                    text: 'Save Game',
-                    setState: {},
-                    saveGame: true,
-                    nextText: 2
-                }
-            ]
-        },
-        {
-            id: 2,
-            text: 'You checked your phone to see the time and to your surprise, you saw that it was already 8 in the morning...',
-            options: [
-                {
-                    text: 'go to the window',
-                    nextText: 3
-                },
-                {
-                    text: 'User your phone to contact your friend',
-                    nextText: 3.1
-                }
-        ]
-        }
-    ]
+    } */
 
     function startGame() {
         state = []
@@ -141,9 +70,104 @@ inside an array which will serve as elements for dialogue and options */
 
 });
 
+// ==== GLOBAL  ==== //
 
+function showTextNode(textNodeIndex) {
+    const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+    textElement.innerText = textNode.text
+    while (optionButtonsElement.firstChild) { // removes buttons
+        optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+    }
+    textNode.options.forEach(option => {
+        if (showOption(option)) {
+            const button = document.createElement('button');
+            button.innerText = option.text;
+            button.classList.add('button');
+            button.addEventListener('click', () => selectOption(option));
+            optionButtonsElement.appendChild(button);
+        }
+    })
+}
 
+/* Takes current state, add everything from `option.setState` to it
+and over-rides everything thats there. So if "Whatever item" is true, but false in `option.setState`
+its going to set it to false in `state` and it will return a brand new object which we will set
+to the current state. */
+function selectOption(option) {
+    const nextTextNodeId = option.nextText;
+    state = Object.assign(state, option.setState);
+    state.textNodeIndex = nextTextNodeId;
+    if (option.saveGame) {
+        saveGame(state);
+    } else {    // Auto Save
+        saveGame(state);
+    }
+    showTextNode(nextTextNodeId);
+}
 
+function saveGame(state) {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem('savedGameState', state[`textNodeIndex`]); // Saves textNodeIndex as state
+        console.log('Game saved:', state);
+        console.log('Serialized state:', serializedState);
+    } catch (error) {
+        console.error('Error while saving the game:', error);
+    }
+}
+
+function showOption(option) {
+    return option.requiredState == null || option.requiredState(state)
+}
+
+const textNodes = [
+    {
+        id: 1,
+        text: 'You suddenly woke up and looked at the window to see the skies were dark. You thought to yourself "Huh? Must still be night time"... ',
+        options: [
+            {
+                text: 'Grab your phone and stand up',
+                setState: { phone: true },
+                nextText: 2
+            },
+            {
+                text: 'Go back to sleep',
+                nextText: 2.1
+            }
+        ]
+    },
+    {
+        id: 2.1,
+        text: 'You woke up once again.. You looked to the window and saw that it was still dark outside... You found it strange so you sat uup on your bed.',
+        options: [
+            {
+                text: 'Grab your phone and stand up',
+                setState: { phone: true },
+                nextText: 2
+            },
+            {
+                text: 'Save Game',
+                setState: {},
+                saveGame: true,
+                nextText: 2,
+            }
+        ]
+    },
+    {
+        id: 2,
+        text: 'You checked your phone to see the time and to your surprise, you saw that it was already 8 in the morning...',
+        options: [
+            {
+                text: 'go to the window',
+                nextText: 3
+            },
+            {
+                text: 'User your phone to contact your friend',
+                nextText: 3.1
+            }
+    ]
+    }
+]
 
 
 /* 
